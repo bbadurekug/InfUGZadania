ce7ff31d4da2c66a2bc238e925d0e8e55a29517c
Błażej Badurek <b.badurek.053@studms.ug.edu.pl>
2023-11-05 11:39:29 +0100

    Zadanie4

---
+++ Lab4/Zadanie4/Zadanie4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main(){
+
+    int dlugosc;
+    int wartosc;
+
+    printf("Jak długa ma być tablica?\n");
+    scanf("%d", &dlugosc);
+
+    int tablica[dlugosc];
+
+    for(int i = 0; i < dlugosc; i++){
+
+        printf("Podaj wartość %d elementu tablicy: \n", i);
+        scanf(" %d", &wartosc);
+        tablica[i] = wartosc;
+
+    }
+
+    printf("Twoja tablica to:\n");
+
+    for(int i = 0; i < dlugosc; i++){
+
+        printf("%d\n", tablica[i]);
+
+    }
+
+    return 0;
+
+}
