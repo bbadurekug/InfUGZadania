e1c6cf43db4873eaaa51b2e95850c13307f11f19
bbadurekug <b.badurek.053@studms.ug.edu.pl>
2023-11-05 22:31:04 +0100

    Zadanie6

--- Lab4/Zadanie6/Zadanie6.c
+++ Lab4/Zadanie6/Zadanie6.c
@@ -3,7 +3,7 @@
 int main(){
 
     int kolumny,rzedy;
-    int wartosc_kolumna, wartosc_rzad;
+    int wartosc_kolumna, wartosc_rzad, wartosc;
 
     printf("Podaj ilosc rzedow w tablicy dwuwymiarowej: \n");
     scanf("%d", &rzedy);
@@ -16,25 +16,58 @@ int main(){
 
         for(int i1 = 0; i1 < kolumny ; i1++){
 
-            printf("Podaj wartość 0 lub 1 dla: i[%d][%d]", i0, i1);
-            scanf("%d", &wartosc_rzad);
-            scanf("%d", &wartosc_kolumna);
-            //tablica[i0][i1] = {wartosc_rzad, wartosc_kolumna};
+            printf("Podaj wartosc 0 lub 1 dla: i[%d][%d]", i0, i1);
+            scanf("%d", &wartosc);
+            if(wartosc){
+
+                wartosc = 120;
+                tablica[i0][i1] = wartosc;
+
+            }
+            else{
+
+                tablica[i0][i1] = wartosc;
+
+            }
 
         }
 
     }
+
+    printf("| ");
+
+    for(int i1 = 0; i1 < kolumny ; i1++){
+
+        printf("- ");
+
+    }
+
+    printf("|\n");
 
 
     for(int i0 = 0; i0 < rzedy ; i0++){
 
+        printf("| ");
+
         for(int i1 = 0; i1 < kolumny ; i1++){
 
-            printf("%d", tablica[i0][i1]);
+            printf("%c ", tablica[i0][i1]);
 
         }
 
+        printf("|\n");
+
     }
+
+    printf("| ");
+
+    for(int i1 = 0; i1 < kolumny ; i1++){
+
+        printf("- ");
+
+    }
+
+    printf("|\n");
 
     return 0;
 
