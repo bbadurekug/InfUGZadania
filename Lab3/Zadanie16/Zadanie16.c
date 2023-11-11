17b6697577f5d4a97734c1c0a868935954aae8e1
bbadurekug <b.badurek.053@studms.ug.edu.pl>
2023-11-11 15:24:23 +0100

    Zadanie16 v2

--- Lab3/Zadanie16/Zadanie16.c
+++ Lab3/Zadanie16/Zadanie16.c
@@ -6,12 +6,34 @@ int main(){
 
     int pomocna = 0, sumaZgadniec = 0;
 
+    short zgadniecie[4] = {0,0,0,0};
+
     printf("Zgadnij liczbe od 1 do 5:\n");
 
     for(int i = 0; i < 4; i++){
 
+        label:
+
         scanf("%d", &pomocna);
-        printf("Zla odpowiedz!\n");
+
+        while(pomocna > 5){
+
+            printf("Podana liczba jest wieksza od 5. Sprobuj ponownie:\n");
+            goto label;
+
+        }
+
+        for(int i = 0; i < 4; i++){
+
+            if(pomocna == zgadniecie[i]){
+
+                printf("Juz probowales te liczbe. Wybierz inna:\n");
+                goto label;
+            }
+
+        }
+
+        zgadniecie[i] = pomocna;
         sumaZgadniec += pomocna;
 
     }
