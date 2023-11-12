4e9cfe1e992cbedff6938d5c473a524804826f94
bbadurekug <b.badurek.053@studms.ug.edu.pl>
2023-11-12 15:53:15 +0100

    Prawie na miejscu

--- Matematyka/Algorytm1.c
+++ Matematyka/Algorytm1.c
@@ -20,30 +20,83 @@ int czyAjestwB(int a, int b){
 
 }
 
+int usuwanieWiekszych(int cyfra, int liczbaSprawdzana, int n){
+
+    int dlugoscTab = (int)log10(liczbaSprawdzana)+1;
+
+    int wieksze[dlugoscTab];
+
+
+    for(int i = 0; i < n; i++){
+
+        if((liczbaSprawdzana/(int)pow(10, n-(i+1)) - liczbaSprawdzana/(int)pow(10, n-i) * 10) > cyfra){
+
+            wieksze[i] = 1;
+
+        }
+        else
+            wieksze[i] = 0;
+
+    }
+
+    //SPRAWDZA KTORE CYFRY W LICZBIE SA WIEKSZE
+
+    int nowaLiczba, staraLiczba;
+
+    staraLiczba = liczbaSprawdzana;
+
+    for(int j = 0; j < n; j++){
+
+        if(wieksze[j] == 1){
+
+            staraLiczba -= staraLiczba/(int)pow(10, n-(j+1)) - staraLiczba/(int)pow(10, n-j) * 10;
+            staraLiczba /= 10;
+
+        }
+
+    }
+
+    nowaLiczba = staraLiczba;
+
+    return nowaLiczba;
+
+    //USUWA TE CYFRY KTORE SA WIEKSZE
+
+}
+
 
 void obliczPodzbiory(short zbior[], int n){
 
     int podzbiory[(int)pow(2,n)];
 
-    podzbiory[0] = 157;
+    podzbiory[0] = 0;
 
     for(int i = 1; i < (int)pow(2,n); i++){
 
-        if(!(czyAjestwB(zbior[n-i], podzbiory[i-1]))){
+        if(!(czyAjestwB(zbior[n-1], podzbiory[i-1]))){
 
-            podzbiory[i] = zbior[n-i];
+            podzbiory[i] = zbior[n-1];
 
         }
         else{
 
-            podzbiory[i] = zbior[n-(i-1)];
+            podzbiory[i] = usuwanieWiekszych(zbior[n-2],podzbiory[i-1],n) * 10 + zbior[n-2];
 
         }
+
     }
+
+
+
+
+    podzbiory[0] = 157;
 
     for(int i=0; i < (int)pow(2,n); i++){
 
-        printf("%d ", podzbiory[i]);
+        if(i)
+            printf("%d ", podzbiory[i]);
+        else
+            printf("%c ", podzbiory[i]);
     }
 
 }
