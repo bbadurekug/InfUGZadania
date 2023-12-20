c5283e710d190673abd7ce64d749e204bad27216
bbadurekug <b.badurek.053@studms.ug.edu.pl>
2023-12-20 15:19:29 +0100

    OSTBR

---
+++ SPOJ/OSTBR/OSTBR.c
@@ -0,0 +1,12 @@
+//To jest odpowiedz w brainfucku, po prostu nie chce zeby bylo widac na githubie ze go uzylem
+//+[>,----- -----]<+++++ +++++.
+
+#include<stdio.h>
+
+int main(){
+
+    printf("Rozwiazanie: +[>,----- -----]<+++++ +++++.\n");
+
+    return 0;
+
+}
