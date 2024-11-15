56b8675ad9275664c408f7840c85f23d717b6394
bbadurekug <149291767+bbadurekug@users.noreply.github.com>
2024-11-15 21:27:13 +0100

    Update RPNTest.java

--- Java/stack/src/RPNTest.java
+++ Java/stack/src/RPNTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 public class RPNTest {
 
 	@org.junit.Test
-	public void testInsertSingle() {
+	public void testExpression1() {
 
         RPN rpn = new RPN();
 
@@ -14,6 +14,42 @@ public class RPNTest {
         String result = rpn.peekStack();
 
 		assertEquals("40", result);
+	}
+
+	@org.junit.Test
+	public void testExpression2() {
+
+        RPN rpn = new RPN();
+
+        rpn.calculateExpression("5 1 2 + 4 x + 3 -");
+
+        String result = rpn.peekStack();
+
+		assertEquals("14", result);
+	}
+
+	@org.junit.Test
+	public void testExpression3() {
+
+        RPN rpn = new RPN();
+
+        rpn.calculateExpression("2 3 + 5 x");
+
+        String result = rpn.peekStack();
+
+		assertEquals("25", result);
+	}
+
+	@org.junit.Test
+	public void testExpression4() {
+
+        RPN rpn = new RPN();
+
+        rpn.calculateExpression("2 7 + 3 / 14 3 - 4 x +");
+
+        String result = rpn.peekStack();
+
+		assertEquals("47", result);
 	}
 
 }
