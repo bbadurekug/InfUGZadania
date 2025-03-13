c7d4ebcda0461012deafe2c45fed749abd525677
Blazej <blazej.badurek.053@studms.ug.edu.pl>
2025-03-13 23:08:33 +0100

    Szyfrowanie done, odszyfrowanie brakuje nwd

--- Kryptografia/1CezarAfiniczny/source.c
+++ Kryptografia/1CezarAfiniczny/source.c
@@ -4,7 +4,13 @@
 #include<stdlib.h>
 #include<string.h>
 
-void szyfrowanie(){
+int nwd(int a, int b){
+
+    //dodac algorytm na nwd
+
+}
+
+void szyfrowanie(char type){
 
     FILE *plain = NULL;
     plain = fopen("./plain.txt", "r");
@@ -18,24 +24,182 @@ void szyfrowanie(){
     char inputChar = '\0';
 
     while(inputChar != '\n' && inputChar != EOF){
-        
+
         inputChar = fgetc(plain);
         input[inputSize - 1] = inputChar;
         input = realloc(input, inputSize++ * sizeof(char));
+
     }
-    
+
     input[inputSize - 1] = '\0';
 
-    printf("%s\n", input);
-    
+    //printf("%s", input);
+
+    FILE *key = NULL;
+    key = fopen("./key.txt", "r");
+
+    int offset = 0;
+    int factor = 0;
+
+    fscanf(key, "%d", &offset);
+
+    if(type == 'a'){
+
+        fscanf(key, "%d", &factor);
+
+        //printf("%d %d\n", factor, offset);
+
+        for(int i = 0; i < strlen(input) - 1; i++){
+
+            if(input[i] >= 65 && input[i] <= 90){
+
+                input[i] = (((input[i] - 65) * factor + offset) % 26) + 65;
+
+            }
+            else if(input[i] >= 97 && input[i] <= 122){
+
+                input[i] = (((input[i] - 97) * factor + offset) % 26) + 97;
+
+            }
+
+        }
+
+    }
+    else{
+
+        for(int i = 0; i < strlen(input) - 1; i++){
+
+            if(input[i] >= 65 && input[i] <= 90){
+
+                input[i] = ((input[i] - 65 + offset) % 26) + 65;
+
+            }
+            else if(input[i] >= 97 && input[i] <= 122){
+
+                input[i] = ((input[i] - 97 + offset) % 26) + 97;
+
+            }
+
+        }
+
+    }
+
+    //printf("%s", input);
+
     //czytanie napisu litera po literze zeby mozna bylo dynamicznie dobra dlugosc tekstu
-    
+
+    FILE *crypto = NULL;
+
+    crypto = fopen("./crypto.txt", "w");
+
+    fprintf(crypto, "%s", input);
+
+    plain = NULL;
+    key = NULL;
+    crypto = NULL;
+
+    free(input);
+
+}
+
+void odszyfrowanie(char type){
+
+    FILE *crypto = NULL;
+    crypto = fopen("./crypto.txt", "r");
+
+    //zrobic tak zeby string.h wykrywalo dlugosc tekstu
+
+    char* input;
+    int inputSize = 1;
+    input = malloc(inputSize * sizeof(char));
+
+    char inputChar = '\0';
+
+    while(inputChar != '\n' && inputChar != EOF){
+
+        inputChar = fgetc(crypto);
+        input[inputSize - 1] = inputChar;
+        input = realloc(input, inputSize++ * sizeof(char));
+
+    }
+
+    input[inputSize - 1] = '\0';
+
+    //printf("%s", input);
+
+    FILE *key = NULL;
+    key = fopen("./key.txt", "r");
+
+    int offset = 0;
+    int factor = 0;
+
+    fscanf(key, "%d", &offset);
+
+    if(type == 'a'){
+
+        fscanf(key, "%d", &factor);
+
+        //to check if the key is correct
+
+        //printf("%d %d\n", factor, offset);
+
+        int revFactor = 0;
+
+        for(int i = 0; i < strlen(input) - 1; i++){
+
+            if(input[i] >= 65 && input[i] <= 90){
+
+                input[i] = ((((input[i] - 65 - offset) * revFactor) % 26 + 26) % 26) + 65;
+
+            }
+            else if(input[i] >= 97 && input[i] <= 122){
+
+                input[i] = ((((input[i] - 97 - offset) * revFactor) % 26 + 26) % 26) + 97;
+
+            }
+
+        }
+
+    }
+    else{
+
+        for(int i = 0; i < strlen(input) - 1; i++){
+
+            if(input[i] >= 65 && input[i] <= 90){
+
+                input[i] = (((input[i] - 65 - offset) % 26 + 26) % 26) + 65;
+
+            }
+            else if(input[i] >= 97 && input[i] <= 122){
+
+                input[i] = (((input[i] - 97 - offset) % 26 + 26) % 26) + 97;
+
+            }
+
+        }
+
+    }
+
+    //printf("%s", input);
+
+    //czytanie napisu litera po literze zeby mozna bylo dynamicznie dobra dlugosc tekstu
+
+    FILE *decrypt = NULL;
+
+    decrypt = fopen("./decrypt.txt", "w");
+
+    fprintf(decrypt, "%s", input);
+
+    decrypt = NULL;
+    key = NULL;
+    crypto = NULL;
+
     free(input);
 
 }
 
 int main(int argc, char *argv[]){
-    
+
     if(argc != 3){
 
         fprintf(stderr, "Nie podano zadnej lub zbyt malo opcji. Podaj dwie opcje. Pierwsza -c dla szyfru cezara lub -a dla szyfru afinicznego i druga -e dla szyfrowania, -d dla odszyfrowywania, -k dla kryptoanalizy z kryptogramem lub -j dla kryptoanalizy z tekstem jawnym");
@@ -48,14 +212,14 @@ int main(int argc, char *argv[]){
     switch(argv[1][1]){
 
         case 'c':
-            
+
             type = 'c';
             printf("Cezar\n");
-            
+
             break;
 
         case 'a':
-            
+
             type = 'a';
             printf("Afiniczny\n");
 
@@ -83,7 +247,9 @@ int main(int argc, char *argv[]){
         case 'd':
 
             printf("Odszyfrowanie\n");
-    
+
+            odszyfrowanie(type);
+
             break;
 
         case 'k':
