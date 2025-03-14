2e3c419ed47081bfc94ba34c2c4b39905cf27dec
Blazej <blazej.badurek.053@studms.ug.edu.pl>
2025-03-14 16:43:32 +0100

    all done

--- Kryptografia/1CezarAfiniczny/source.c
+++ Kryptografia/1CezarAfiniczny/source.c
@@ -21,6 +21,18 @@ int nwd(int a, int b){
 
 }
 
+int stringCompare(char* longer, char* shorter){
+
+    for(int i = 0; i < strlen(shorter) - 1; i++){
+
+        if(longer[i] != shorter[i]) return 0;
+
+    }
+
+    return 1;
+
+}
+
 void szyfrowanie(char type){
 
     FILE *plain = NULL;
@@ -57,6 +69,13 @@ void szyfrowanie(char type){
     if(type == 'a'){
 
         fscanf(key, "%d", &factor);
+
+        if(nwd(26, factor) != 1){
+
+            fprintf(stderr, "Niepoprawny klucz! NWD(26,a) nie jest rowny 1!");
+            return;
+
+        }
 
         //printf("%d %d\n", factor, offset);
 
@@ -156,7 +175,7 @@ void odszyfrowanie(char type){
 
         if(nwd(26, factor) != 1){
 
-            fprintf(stderr, "NWD(26,a) nie jest rowny 1!");
+            fprintf(stderr, "Niepoprawny klucz! NWD(26,a) nie jest rowny 1!");
             return;
 
         }
@@ -173,6 +192,7 @@ void odszyfrowanie(char type){
             else if(revFactor != -1 && (factor * i) % 26 == 1){
 
                 fprintf(stderr, "Istnieje wiele odwrotnych liczb!");
+                free(input);
                 return;
 
             }
@@ -333,6 +353,236 @@ void kryptogram(char type){
 }
 
 void jawny(char type){
+
+    FILE *crypto = NULL;
+    crypto = fopen("./crypto.txt", "r");
+
+    //zrobic tak zeby string.h wykrywalo dlugosc tekstu
+
+    char* input;
+    int inputSize = 1;
+    input = malloc(inputSize * sizeof(char));
+
+    char inputChar = '\0';
+
+    while(inputChar != '\n' && inputChar != EOF){
+
+        inputChar = fgetc(crypto);
+        input[inputSize - 1] = inputChar;
+        input = realloc(input, inputSize++ * sizeof(char));
+
+    }
+
+    input[inputSize - 1] = '\0';
+
+    FILE *extra = NULL;
+
+    extra = fopen("./extra.txt", "r");
+
+    char* inputExtra;
+    int inputExtraSize = 1;
+    inputExtra = malloc(inputExtraSize * sizeof(char));
+
+    inputChar = '\0';
+
+    while(inputChar != '\n' && inputChar != EOF){
+
+        inputChar = fgetc(extra);
+        inputExtra[inputExtraSize - 1] = inputChar;
+        inputExtra = realloc(inputExtra, inputExtraSize++ * sizeof(char));
+
+    }
+
+    inputExtra[inputExtraSize - 1] = '\0';
+
+    //printf("%s%s", input, inputExtra);
+
+    char* inputCopy;
+    inputCopy = malloc(strlen(input) + 1);
+
+    char* inputMatch;
+    inputMatch = malloc(strlen(input) + 1);
+
+    int offsetFound = -1;
+    int revFactor = -1;
+    int factorFound = -1;
+
+    if(type == 'a'){
+
+        int dividers[] = {1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25};
+
+        strcpy(inputCopy, input);
+
+        for(int i = 0; i < 12; i++){
+
+            for(int offset = 0; offset < 26; offset++){
+
+                if(i == 0 && offset == 0) offset = 1;
+
+                for(int j = 0; j < strlen(input) - 1; j++){
+
+                    if(inputCopy[j] >= 65 && inputCopy[j] <= 90){
+
+                        inputCopy[j] = ((((inputCopy[j] - 65 - offset) * dividers[i]) % 26 + 26) % 26) + 65;
+
+                    }
+                    else if(inputCopy[j] >= 97 && inputCopy[j] <= 122){
+
+                        inputCopy[j] = ((((inputCopy[j] - 97 - offset) * dividers[i]) % 26 + 26) % 26) + 97;
+
+                    }
+
+                }
+
+                if(stringCompare(inputCopy, inputExtra)){
+
+                    //printf("%d ", offset);
+
+                    if(offsetFound == -1 && factorFound == -1){
+
+                        offsetFound = offset;
+
+                        revFactor = dividers[i];
+
+                        for(int i = 0; i < 26; i++){
+
+                            if(factorFound == -1 && (revFactor * i) % 26 == 1){
+
+                                factorFound = i;
+
+                            }
+                            else if(factorFound != -1 && (revFactor * i) % 26 == 1){
+
+                                fprintf(stderr, "Istnieje wiele odwrotnych liczb!");
+                                free(inputMatch);
+                                free(inputCopy);
+                                free(input);
+                                return;
+
+                            }
+
+                        }
+
+                        strcpy(inputMatch, inputCopy);
+
+                    }
+                    else{
+
+                        fprintf(stderr, "Nie mozna zalezc klucza!");
+
+                        free(inputMatch);
+                        free(inputCopy);
+                        free(input);
+
+                        return;
+
+                    }
+
+                }
+
+                strcpy(inputCopy, input);
+
+            }
+
+        }
+
+        if(offsetFound == -1 && factorFound == -1){
+
+            fprintf(stderr, "Nie mozna zalezc klucza!");
+
+            free(inputMatch);
+            free(inputCopy);
+            free(input);
+
+            return;
+
+        }
+
+    }
+    else{
+
+        strcpy(inputCopy, input);
+
+        for(int offset = 1; offset < 26; offset++){
+
+            for(int i = 0; i < strlen(input) - 1; i++){
+
+                if(inputCopy[i] >= 65 && inputCopy[i] <= 90){
+
+                    inputCopy[i] = (((inputCopy[i] - 65 - offset) % 26 + 26) % 26) + 65;
+
+                }
+                else if(inputCopy[i] >= 97 && inputCopy[i] <= 122){
+
+                    inputCopy[i] = (((inputCopy[i] - 97 - offset) % 26 + 26) % 26) + 97;
+
+                }
+
+            }
+
+            //printf("%s%s---------------\n", inputCopy, inputExtra);
+
+            if(stringCompare(inputCopy, inputExtra)){
+
+                //printf("%d ", offset);
+
+                if(offsetFound == -1){
+
+                    offsetFound = offset;
+                    strcpy(inputMatch, inputCopy);
+
+                }
+                else{
+
+                    fprintf(stderr, "Nie mozna zalezc klucza!");
+
+                    free(inputMatch);
+                    free(inputCopy);
+                    free(input);
+
+                    return;
+
+                }
+
+            }
+
+            strcpy(inputCopy, input);
+
+        }
+
+        if(offsetFound == -1){
+
+            fprintf(stderr, "Nie mozna zalezc klucza!");
+
+            free(inputMatch);
+            free(inputCopy);
+            free(input);
+
+            return;
+
+        }
+
+    }
+
+    FILE *keyFound = NULL;
+    keyFound = fopen("./key-found.txt", "w");
+
+    fprintf(keyFound, "%d", offsetFound);
+
+    if(type == 'a') fprintf(keyFound, " %d", factorFound);
+
+    FILE *decrypt = NULL;
+    decrypt = fopen("./decrypt.txt", "w");
+    fprintf(decrypt, "%s", inputMatch);
+
+    decrypt = NULL;
+    keyFound = NULL;
+    crypto = NULL;
+    extra = NULL;
+
+    free(inputMatch);
+    free(inputCopy);
+    free(input);
 
 }
 
