c08588441046fcf09ab3e387f198e4d55c4f0908
Blazej <blazej.badurek.053@studms.ug.edu.pl>
2025-03-27 23:06:12 +0100

    dodac podlewanie wielu roslin i powiadomienia

--- TechnikiInternetowe/3Zadanie/script.js
+++ TechnikiInternetowe/3Zadanie/script.js
@@ -1,5 +1,12 @@
 let rows = 2;
 let columns = 5;
+let noPlants = 3;
+
+const plantsToChoose = new Array(noPlants);
+
+plantsToChoose[0] = "sunflower";
+plantsToChoose[1] = "pea";
+plantsToChoose[2] = "garlic";
 
 const plants = new Array(rows).fill().map(() => new Array(columns));
 
@@ -7,89 +14,183 @@ for(let i = 0; i < rows; i++){
 
 	for(let j = 0; j < columns; j++){
 
-		plants[i][j] = "None";
+		plants[i][j] = ["none", 0];
 
 	}
 
 }
 
-function spawnSunflower(){
+function getRandomPlant(){
+    return plantsToChoose[Math.floor(Math.random() * noPlants)];
+}
+
+function isMoreSpace(){
     
+    let oneNone = false;
+        
+    for(let i = 0; i < rows; i++){
+
+	    for(let j = 0; j < columns; j++){
+
+	    	if (plants[i][j][0] == "none") {
+                
+                oneNone = true;
+                break;
+
+            }
+
+	    }
+
+    }   
+
+    return oneNone;
+
+}
+
+function updatePlant(row, column){
+
+    document.getElementById("row" + row).querySelector("#column" + column + "plant").innerHTML = '<img src="assets/' +  plants[row-1][column-1][0] + '.png" style="scale: ' +  plants[row-1][column-1][1] + '%; bottom: ' + (-48 - (0.16 * plants[row-1][column-1][1])) + 'px" alt="' + plants[row-1][column-1][0] + '">'; 
+
+}
+
+function isFirstPlant(){
+
+    let allNone = true;
+
+    for(let i = 0; i < rows; i++){
+
+	    for(let j = 0; j < columns; j++){
+
+	    	if (plants[i][j][0] != "none") {
+                
+                allNone = false;
+                break;
+
+            }
+
+	    }
+
+    }   
+
+    return allNone;
+
+}
+
+function spawnPlant(plant){
+    
+    if (isFirstPlant() == false){
+
+        return;
+
+    }
+
     console.log(document.getElementById("rowIndex").value);
     console.log(document.getElementById("columnIndex").value);
     
     row = document.getElementById("rowIndex").value;
     column = document.getElementById("columnIndex").value;
 
-    if (plants[row-1][column-1] == "None") {
+    if (plants[row-1][column-1][0] == "none") {
+        
+        plants[row-1][column-1][0] = plant;
+        plants[row-1][column-1][1] = 10;
 
-        document.getElementById("row" + row).querySelector("#column" + column + "plant").innerHTML = '<img src="assets/sunflower.png" width="128" height="128" alt="Obraz przedtawiający słonecznik">'; 
-      
-        plants[row-1][column-1] = "Sunflower"
+        updatePlant(row, column);        
 
-        console.log("Sunflower spawned!");
+        console.log(plant + " spawned!");
     
     }
 
 }
 
-function spawnPea(){
+function spawnPlantRandom(plant){
     
-    console.log(document.getElementById("rowIndex").value);
-    console.log(document.getElementById("columnIndex").value);
-    
-    row = document.getElementById("rowIndex").value;
-    column = document.getElementById("columnIndex").value;
-
-    if (plants[row-1][column-1] == "None") {
-
-        document.getElementById("row" + row).querySelector("#column" + column + "plant").innerHTML = '<img src="assets/pea.png" width="128" height="128" alt="Obraz przedtawiający groszek">'; 
-      
-        plants[row-1][column-1] = "Pea"
-
-        console.log("Pea spawned!");
-    
+    if (isMoreSpace() == false) {
+        
+        return;
     }
 
+    row = Math.floor(Math.random() * rows);
+    column = Math.floor(Math.random() * columns);
+    
+    while (plants[row][column][0] != "none") {
+
+        row = Math.floor(Math.random() * rows);
+        column = Math.floor(Math.random() * columns);
+
+    }
+
+    plants[row][column][0] = plant;
+    plants[row][column][1] = 10;
+
+    updatePlant(row + 1, column + 1);        
+
+    console.log(plant + " spawned!");
+    
 }
 
-function spawnGarlic(){
-    
+
+
+function waterPlant(){
+
     console.log(document.getElementById("rowIndex").value);
     console.log(document.getElementById("columnIndex").value);
     
     row = document.getElementById("rowIndex").value;
     column = document.getElementById("columnIndex").value;
 
-    if (plants[row-1][column-1] == "None") {
+    if(plants[row-1][column-1][1] >= 150) {
+   
+        document.getElementById("row" + row).querySelector("#column" + column + "plant").innerHTML = '<img src="assets/' +  plants[row-1][column-1][0] + '.png" style="scale: ' +  plants[row-1][column-1][1] + '%; bottom: ' + (-48 - (0.16 * plants[row-1][column-1][1])) + 'px; border-radius: 50%; background-color: rgba(255, 0, 0, 0.5)" alt="' + plants[row-1][column-1][0] + '">'; 
 
-        document.getElementById("row" + row).querySelector("#column" + column + "plant").innerHTML = '<img src="assets/garlic.png" width="128" height="128" alt="Obraz przedtawiający czosnek">'; 
-      
-        plants[row-1][column-1] = "Garlic"
+        return;
 
-        console.log("Garlic spawned!");
+    }   
+
+    if (plants[row-1][column-1][0] != "none") {
     
+        plants[row-1][column-1][1] += 10;
+
+        updatePlant(row, column); 
+
     }
 
 }
 
 function removePlant(){
-    
+
     console.log(document.getElementById("rowIndex").value);
     console.log(document.getElementById("columnIndex").value);
     
     row = document.getElementById("rowIndex").value;
     column = document.getElementById("columnIndex").value;
 
-    if (plants[row-1][column-1] != "None") {
+    if (plants[row-1][column-1][0] != "none") {
+        
+        plants[row-1][column-1][0] = "none";
+        plants[row-1][column-1][1] += 0;
 
-        document.getElementById("row" + row).querySelector("#column" + column + "plant").innerHTML = ""; 
-      
-        plants[row-1][column-1] = "None"
+        document.getElementById("row" + row).querySelector("#column" + column + "plant").innerHTML = ''; 
 
-        console.log("Plant removed!");
-    
     }
 
 }
 
+function healPlants(){
+
+    for(let i = 0; i < rows; i++){
+
+        for(let j = 0; j < columns; j++){
+
+	    	if (plants[i][j][1] >= 150) {
+                
+                plants[i][j][1] = 10;
+                updatePlant(i + 1, j + 1);
+
+            }
+
+	    }
+
+    }   
+
+}
