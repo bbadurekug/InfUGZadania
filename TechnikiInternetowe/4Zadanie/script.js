034a088cc97234f729ac36ef599ffd7cd8ea13d2
Błażej Badurek <b.badurek.053@studms.ug.edu.pl>
2025-04-06 11:52:03 +0200

    LaTeX start, wyznacznik macierzy

---
+++ TechnikiInternetowe/4Zadanie/script.js
@@ -0,0 +1,12 @@
+let matrixRows = 1;
+let matrixColumns = 1;
+
+function updateMatrix(){
+
+	document.getElementById("matrixInput").innerHTML = '$\\begin{bmatrix} 1 & 0 \\end{bmatrix}$'; 
+	
+//	MathJax.typeset();
+
+}
+
+updateMatrix();
