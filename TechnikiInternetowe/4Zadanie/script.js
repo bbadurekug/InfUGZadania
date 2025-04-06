b136c233da56b467c736f0ce5d06025c7894f6c0
Błażej Badurek <b.badurek.053@studms.ug.edu.pl>
2025-04-06 12:18:59 +0200

    Dodawanie macierzy

--- TechnikiInternetowe/4Zadanie/script.js
+++ TechnikiInternetowe/4Zadanie/script.js
@@ -1,9 +1,45 @@
-let matrixRows = 1;
-let matrixColumns = 1;
+let matrixRows = 3;
+let matrixColumns = 6;
+
+const matrix = new Array(matrixRows).fill().map(() => new Array(matrixColumns));
+
+for(let i = 0; i < matrixRows; i++){
+
+	for(let j = 0; j < matrixColumns; j++){
+
+		matrix[i][j] = 1;
+
+	}
+
+}
+
+function prepareEmptyMatrix(){
+	
+	let matrixString = "$\\begin{bmatrix}";
+
+	for(let i = 0; i < matrixColumns; i++){
+		
+		for(let j = 0; j < matrixRows; j++){
+			
+			matrixString += matrix[j][i];
+
+			if(j < matrixRows - 1) matrixString += "&";
+
+		}
+
+		matrixString += "\\\\";
+
+	}
+	
+	matrixString += "\\end{bmatrix}$"
+
+	return matrixString;
+
+}
 
 function updateMatrix(){
 
-	document.getElementById("matrixInput").innerHTML = '$\\begin{bmatrix} 1 & 0 \\end{bmatrix}$'; 
+	document.getElementById("matrixInput").innerHTML = prepareEmptyMatrix(); 
 	
 //	MathJax.typeset();
 
