512aa3670360b603b68849ae261d77c24977cb51
bbadurekug <b.badurek.053@studms.ug.edu.pl>
2026-03-22 11:59:00 +0100

    setup

---
+++ DevOps/ReverseProxy/backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PROT || 3000;
+
+app.get('/items', (req, res) => {
+    //definicja
+});
+
+app.post('/items', (req, res) => {
+    //definicja
+});
+
+app.get('/stats', (req, res) => {
+    //definicja
+});
+
+app.listen(PORT, () => {
+    console.log(`Serwer nasłuchuje na porcie: ${PORT}`);
+});
